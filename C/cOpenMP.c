b87865c759653fb397325028f28a5e03ef8b8327
Zehe Zack Luan <70601913+zehezack@users.noreply.github.com>
2021-11-30 17:45:33 -0500

    Add files via upload

---
+++ C/cOpenMP.c
@@ -0,0 +1,41 @@
+//
+//  cSerial.c
+//  CISC372_Project
+//
+//  Created by Zehe Luan on 11/27/21.
+//
+
+#include <math.h>
+#include <stdio.h>
+#include <float.h>
+#include <sys/time.h>
+#include <omp.h>
+
+int main() {
+    struct timeval start, end;
+    int digs = DECIMAL_DIG;
+    long long precision = pow(2,31) - 1;
+    double result = 0;
+    gettimeofday(&start, NULL);
+	
+    #pragma omp parallel
+    {
+    #pragma omp for reduction(+: result)
+    for (long long i=1; i<precision;i+=4)
+            result += (double)1/i;
+    #pragma omp for reduction(-: result)
+    for (long long i=3; i<precision;i+=4)
+            result -= (double)1/i;
+    }
+
+    gettimeofday(&end, NULL);
+    long sec_take = (end.tv_sec-start.tv_sec);
+    long usec_take = (end.tv_usec-start.tv_usec);
+    if (usec_take < 0) {
+            sec_take -= 1;
+            usec_take += 1000000;
+    }
+    printf("Pi is approximately %.*e\n", digs, result*4);
+    printf("Time taken: %ld seconds, %ld microseconds\n", sec_take, usec_take);
+}
+
